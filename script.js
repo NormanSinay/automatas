957d5f55c09a4506d8f7f3217554015d484751d7
Norman Sinay <48865455+NormanSinay@users.noreply.github.com>
2023-09-07 20:30:28 -0600

    Add files via upload
    Se agrega validación para la carga de archivos txt

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-// Función para procesar el contenido del archivo
 function procesarContenido(contenido) {
     const lineas = contenido.split('\n');
     const vectorRectangular = {
@@ -9,40 +7,67 @@ function procesarContenido(contenido) {
         A: [],
         W: []
     };
+    let contenidoValido = true;
 
-    lineas.forEach((linea) => {
-        if (linea.includes('Q=')) {
-            const contenidoQ = linea.match(/\{(.*?)\}/);
-            if (contenidoQ) {
-                vectorRectangular.Q = contenidoQ[1].split(',').map((element) => element.trim());
+    if (
+        !lineas.some(linea => linea.includes('Q=')) ||
+        !lineas.some(linea => linea.includes('F=')) ||
+        !lineas.some(linea => linea.includes('i=')) ||
+        !lineas.some(linea => linea.includes('A=')) ||
+        !lineas.some(linea => linea.includes('W='))
+    ) {
+        contenidoValido = false;
+    }
+
+    if (contenidoValido) {
+        lineas.forEach((linea) => {
+            if (linea.includes('Q=')) {
+                const contenidoQ = linea.match(/\{(.*?)\}/);
+                if (contenidoQ) {
+                    vectorRectangular.Q = contenidoQ[1].split(',').map((element) => element.trim());
+                } else {
+                    contenidoValido = false;
+                }
+            } else if (linea.includes('F=')) {
+                const contenidoF = linea.match(/\{(.*?)\}/);
+                if (contenidoF) {
+                    vectorRectangular.F = contenidoF[1].split(',').map((element) => element.trim());
+                } else {
+                    contenidoValido = false;
+                }
+            } else if (linea.includes('i=')) {
+                const contenidoI = linea.match(/i=([A-Za-z]+)/);
+                if (contenidoI) {
+                    vectorRectangular.i = contenidoI[1];
+                } else {
+                    contenidoValido = false;
+                }
+            } else if (linea.includes('A=')) {
+                const contenidoA = linea.match(/\{(.*?)\}/);
+                if (contenidoA) {
+                    vectorRectangular.A = contenidoA[1].split(',').map((element) => element.trim());
+                } else {
+                    contenidoValido = false;
+                }
+            } else if (linea.includes('W=')) {
+                const contenidoW = linea.match(/\{(.*?)\}/);
+                if (contenidoW) {
+                    vectorRectangular.W = contenidoW[1].split(',').map((element) => element.trim());
+                } else {
+                    contenidoValido = false;
+                }
             }
-        } else if (linea.includes('F=')) {
-            const contenidoF = linea.match(/\{(.*?)\}/);
-            if (contenidoF) {
-                vectorRectangular.F = contenidoF[1].split(',').map((element) => element.trim());
-            }
-        } else if (linea.includes('i=')) {
-            const contenidoI = linea.match(/i=([A-Za-z]+)/);
-            if (contenidoI) {
-                vectorRectangular.i = contenidoI[1];
-            }
-        } else if (linea.includes('A=')) {
-            const contenidoA = linea.match(/\{(.*?)\}/);
-            if (contenidoA) {
-                vectorRectangular.A = contenidoA[1].split(',').map((element) => element.trim());
-            }
-        } else if (linea.includes('W=')) {
-            const contenidoW = linea.match(/\{(.*?)\}/);
-            if (contenidoW) {
-                vectorRectangular.W = contenidoW[1].split(',').map((element) => element.trim());
-            }
-        }
-    });
+        });
+    }
+
+    if (!contenidoValido) {
+        alert("El contenido del archivo no es correcto.");
+        return null; 
+    }
 
     return vectorRectangular;
 }
 
-// Event listener para el input de archivo y el área de arrastrar y soltar
 const fileInput = document.getElementById("input-file-now");
 const contentDisplay = document.getElementById("content-display");
 const qTableBody = document.getElementById("q-table-body");
@@ -51,38 +76,56 @@ const iTableBody = document.getElementById("i-table-body");
 const aTableBody = document.getElementById("a-table-body");
 const wTableBody = document.getElementById("w-table-body");
 const container = document.getElementById("container");
+const errorMessage = document.getElementById("error-message");
 
-// Evitar que el navegador abra el archivo en una ventana al arrastrar
 container.addEventListener("dragover", (e) => {
     e.preventDefault();
 });
 
-// Cambiar el evento change para que también se active al soltar un archivo en el área de arrastrar y soltar
 container.addEventListener("drop", (e) => {
     e.preventDefault();
     fileInput.files = e.dataTransfer.files;
+    validarYProcesarArchivo();
+});
+
+fileInput.addEventListener("change", () => {
+    validarYProcesarArchivo();
+});
+
+function validarYProcesarArchivo() {
     const file = fileInput.files[0];
     if (file) {
         const reader = new FileReader();
         reader.onload = (event) => {
             const contenido = event.target.result;
-            contentDisplay.textContent = contenido;
             const vectorRectangular = procesarContenido(contenido);
 
-            // Mostrar los vectores en las tablas
+            if (!vectorRectangular) {
+                errorMessage.style.display = "block";
+                contentDisplay.textContent = "";
+                qTableBody.innerHTML = "";
+                fTableBody.innerHTML = "";
+                iTableBody.innerHTML = "";
+                aTableBody.innerHTML = "";
+                wTableBody.innerHTML = "";
+                return;
+            }
+
+            errorMessage.style.display = "none";
+            contentDisplay.textContent = contenido;
+
             mostrarVectorEnTabla(vectorRectangular.Q, qTableBody);
             mostrarVectorEnTabla(vectorRectangular.F, fTableBody);
-            mostrarVectorEnTabla([vectorRectangular.i], iTableBody); // El estado inicial es un solo valor
+            mostrarVectorEnTabla([vectorRectangular.i], iTableBody);
             mostrarVectorEnTabla(vectorRectangular.A, aTableBody);
             mostrarMatrizEnTabla(vectorRectangular.W, wTableBody);
         };
         reader.readAsText(file);
     }
-});
+}
 
-// Función para mostrar un vector en una tabla vertical
 function mostrarVectorEnTabla(vector, tbody) {
-    tbody.innerHTML = ""; // Limpiar el tbody
+    tbody.innerHTML = "";
     vector.forEach((element) => {
         const tableRow = document.createElement("tr");
         const tableData = document.createElement("td");
@@ -92,12 +135,11 @@ function mostrarVectorEnTabla(vector, tbody) {
     });
 }
 
-// Función para mostrar la matriz en una tabla
 function mostrarMatrizEnTabla(matriz, tbody) {
-    tbody.innerHTML = ""; // Limpiar el tbody
+    tbody.innerHTML = "";
     let tableRow = document.createElement("tr");
     for (let i = 0; i < matriz.length; i++) {
-        const elemento = matriz[i].replace(/[()]/g, ''); // Eliminar paréntesis
+        const elemento = matriz[i].replace(/[()]/g, '');
         const tableData = document.createElement("td");
         tableData.textContent = elemento;
         tableRow.appendChild(tableData);

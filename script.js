9de81857c12195d5cc0c8fd3fc2f62bd99c1d95f
Norman Sinay <48865455+NormanSinay@users.noreply.github.com>
2023-09-19 21:35:11 -0600

    AutomatasV2.1
    AutomatasV2.1

--- script.js
+++ script.js
@@ -52,7 +52,11 @@ function procesarContenido(contenido) {
             } else if (linea.includes('W=')) {
                 const contenidoW = linea.match(/\{(.*?)\}/);
                 if (contenidoW) {
-                    vectorRectangular.W = contenidoW[1].split(',').map((element) => element.trim());
+                    const matrizW = contenidoW[1].split('),').map((element) => element.trim());
+                    vectorRectangular.W = matrizW.map((fila) => {
+                        const elementos = fila.split(',');
+                        return elementos.map((element) => element.trim().replace(/[()]/g, ''));
+                    });
                 } else {
                     contenidoValido = false;
                 }
@@ -62,14 +66,13 @@ function procesarContenido(contenido) {
 
     if (!contenidoValido) {
         alert("El contenido del archivo no es correcto.");
-        return null; 
+        return null;
     }
 
-    return vectorRectangular;
+    return contenidoValido ? vectorRectangular : null;
 }
 
 const fileInput = document.getElementById("input-file-now");
-const contentDisplay = document.getElementById("content-display");
 const qTableBody = document.getElementById("q-table-body");
 const fTableBody = document.getElementById("f-table-body");
 const iTableBody = document.getElementById("i-table-body");
@@ -77,6 +80,7 @@ const aTableBody = document.getElementById("a-table-body");
 const wTableBody = document.getElementById("w-table-body");
 const container = document.getElementById("container");
 const errorMessage = document.getElementById("error-message");
+const contentCard = document.getElementById("content-card");
 
 container.addEventListener("dragover", (e) => {
     e.preventDefault();
@@ -92,36 +96,79 @@ fileInput.addEventListener("change", () => {
     validarYProcesarArchivo();
 });
 
+function agregarTitulo(tablaId, titulo) {
+    const tabla = document.getElementById(tablaId);
+    // Verificar si ya existe un título en la tabla y eliminarlo si es el caso
+    const tituloExistente = tabla.querySelector('caption');
+    if (tituloExistente) {
+        tituloExistente.remove();
+    }
+    const caption = document.createElement('caption');
+    caption.textContent = titulo;
+    tabla.appendChild(caption);
+}
+
+
+function mostrarElemento(elemento) {
+    elemento.style.display = "block";
+}
+
+function ocultarElemento(elemento) {
+    elemento.style.display = "none";
+}
+
 function validarYProcesarArchivo() {
     const file = fileInput.files[0];
     if (file) {
         const reader = new FileReader();
         reader.onload = (event) => {
             const contenido = event.target.result;
+            const contentDisplay = document.getElementById("content-display");
+            contentDisplay.textContent = contenido;
             const vectorRectangular = procesarContenido(contenido);
 
             if (!vectorRectangular) {
                 errorMessage.style.display = "block";
-                contentDisplay.textContent = "";
-                qTableBody.innerHTML = "";
-                fTableBody.innerHTML = "";
-                iTableBody.innerHTML = "";
-                aTableBody.innerHTML = "";
-                wTableBody.innerHTML = "";
+                contentCard.style.display = "none"; // Oculta el contenido del archivo
+                eliminarTitulosColumnas();
+                limpiarTablaW(); // Agrega esta línea para limpiar la tabla W
                 return;
             }
 
             errorMessage.style.display = "none";
-            contentDisplay.textContent = contenido;
+            mostrarElemento(contentCard); // Muestra el contenido del archivo
+            eliminarTitulosColumnas();
+            limpiarTablaW(); // Agrega esta línea para limpiar la tabla W
+
+            agregarTitulo('q-table', 'Estados (Q)');
+            agregarTitulo('f-table', 'Alfabeto (F)');
+            agregarTitulo('i-table', 'Estado inicial (i)');
+            agregarTitulo('a-table', 'Estado de Aceptación (A)');
+            agregarTitulo('w-table', 'Matriz de Transición (W)');
 
             mostrarVectorEnTabla(vectorRectangular.Q, qTableBody);
             mostrarVectorEnTabla(vectorRectangular.F, fTableBody);
             mostrarVectorEnTabla([vectorRectangular.i], iTableBody);
             mostrarVectorEnTabla(vectorRectangular.A, aTableBody);
-            mostrarMatrizEnTabla(vectorRectangular.W, wTableBody);
+            mostrarMatrizEnTabla(vectorRectangular.W, wTableBody, vectorRectangular.F);
         };
         reader.readAsText(file);
     }
+}
+
+function eliminarTitulosColumnas() {
+    const tablas = document.querySelectorAll('table');
+    tablas.forEach(tabla => {
+        const caption = tabla.querySelector('caption');
+        if (caption) {
+            caption.remove();
+        }
+    });
+}
+
+function limpiarTablaW() {
+    const wTableBody = document.getElementById("w-table-body");
+    wTableBody.innerHTML = ""; // Elimina el contenido anterior de la tabla W
 }
 
 function mostrarVectorEnTabla(vector, tbody) {
@@ -135,17 +182,40 @@ function mostrarVectorEnTabla(vector, tbody) {
     });
 }
 
-function mostrarMatrizEnTabla(matriz, tbody) {
-    tbody.innerHTML = "";
-    let tableRow = document.createElement("tr");
-    for (let i = 0; i < matriz.length; i++) {
-        const elemento = matriz[i].replace(/[()]/g, '');
-        const tableData = document.createElement("td");
-        tableData.textContent = elemento;
-        tableRow.appendChild(tableData);
-        if ((i + 1) % 3 === 0 || i === matriz.length - 1) {
-            tbody.appendChild(tableRow);
-            tableRow = document.createElement("tr");
-        }
+function mostrarMatrizEnTabla(matriz, tbody, encabezados) {
+    // Verificar si ya existe un encabezado de columna, y si no, agregar uno
+    if (tbody.querySelector("th") === null) {
+        const headerRow = document.createElement("tr");
+        headerRow.innerHTML = "<th>Estado</th>";
+
+        encabezados.forEach((element) => {
+            const tableHeader = document.createElement("th");
+            tableHeader.textContent = element;
+            headerRow.appendChild(tableHeader);
+        });
+
+        tbody.appendChild(headerRow);
     }
+
+    // Agregar filas de la matriz
+    matriz.forEach((fila) => {
+        const estado = fila[0].trim();
+        const tableRow = document.createElement("tr");
+        const tableDataEstado = document.createElement("td");
+        tableDataEstado.textContent = estado;
+        tableRow.appendChild(tableDataEstado);
+
+        // Llenar las celdas correspondientes según los encabezados
+        for (let i = 1; i < fila.length; i++) {
+            const elemento = fila[i].trim();
+            const tableData = document.createElement("td");
+            tableData.textContent = elemento;
+            tableRow.appendChild(tableData);
+        }
+
+        tbody.appendChild(tableRow);
+    });
 }
+
+// Oculta el contenido del archivo al cargar la página
+ocultarElemento(contentCard);
